18c053763ba4ddc27f80f074b5c00d2c504d4f0d
AD001\z004efdb <shreya.malviya@siemens.com>
2023-06-14 09:03:36 +0530

    [Shreya] Add: Git demo

---
+++ GitDemo/GitDemo/GitDemoCalss.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    public class GitDemoCalss
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Hi All");
+        }
+    }
+}
